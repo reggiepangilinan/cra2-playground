3d4eeb8e95d56b369e96002fb42bd2dd21ef8d80
Reggie Pangilinan <reggiepangilinan@gmail.com>
2019-02-05 23:20:24 +1100

    Add form validations using yup

--- src/features/sampleform/sampleform.component.tsx
+++ src/features/sampleform/sampleform.component.tsx
@@ -1,53 +1,61 @@
 import React, { FunctionComponent } from 'react'
 import { Formik, FormikProps, Form, Field, FieldProps } from 'formik';
-type Props =  {
+import styles from './sampleform.module.scss';
+import {string, object } from 'yup'
+
+type Props = {
 
 }
 
-
 export type SampleFormModel = {
-    name: string,
-    email: string
+  name: string,
+  email: string,
+  sendMeUpdates: boolean
 };
 
+const handleSubmit = (values: SampleFormModel) => {
+  alert(JSON.stringify(values));
+};
 
-const SampleForm: FunctionComponent<Props> = () => { 
-    return (
-        <div>
-            <h3>Sample form</h3>
-            <Formik
-        initialValues={{ name: '' , email: ''}}
-        onSubmit={(values: SampleFormModel) => alert(JSON.stringify(values))}
-        render={(formikBag: FormikProps<SampleFormModel>) => (
-          <Form>
-            <Field
-              name="name"
-              render={({ field, form }: FieldProps<SampleFormModel>) => (
-                <div>
-                  <input type="text" {...field} placeholder="Name" />
-                  {form.touched.name &&
-                    form.errors.name &&
-                    form.errors.name}
-                </div>
-              )}
-            />
-            <Field
-              name="email"
-              render={({ field, form }: FieldProps<SampleFormModel>) => (
-                <div>
-                  <input type="text" {...field} placeholder="Email" />
-                  {form.touched.email &&
-                    form.errors.email &&
-                    form.errors.email}
-                </div>
-              )}
-            />
-                   <button type="submit">Submit</button>
+const validatonSchema = object().shape({
+  name: string().min(9, 'Name must be 9 characters or longer').required('Name is required'),
+  email: string().email('Email not valid').required('Email is required'),
+});
+
+const SampleForm: FunctionComponent<Props> = () => {
+  return (
+    <div>
+      <h3>Sample form</h3>
+      <Formik
+        initialValues={{ name: '', email: '', sendMeUpdates: false }}
+        onSubmit={handleSubmit}
+        validationSchema={validatonSchema}
+        render={({ touched, errors, values, submitForm }: FormikProps<SampleFormModel>) => (
+
+          <Form className={styles.sampleForm}>
+            <div className={styles.fieldGroupInput}>
+              <label>Name</label>
+              <Field name="name" placeholder="Name" type="text" />
+              { touched.name && errors.name && <small>{errors.name}</small> }
+              
+            </div>
+
+            <div className={styles.fieldGroupInput}>
+              <label>Email</label>
+              <Field name="email" placeholder="Email" type="email" />
+              { touched.email && errors.email && <small>{errors.email}</small> }
+            </div>
+
+            <label>
+              <Field name="sendMeUpdates" type="checkbox" checked={values.sendMeUpdates} />
+              Send me updates
+            </label>
+            <button type="submit" onClick={()=> submitForm()}>Submit</button>
           </Form>
         )}
       />
-        </div>
-    )
+    </div>
+  )
 }
 
 export default SampleForm;
