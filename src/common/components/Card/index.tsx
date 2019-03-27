20a351a8529de8618de897e8f11d34b85afc0ca7
Reggie <reggie.pangilinan@infotrack.com.au>
2019-03-27 17:02:16 +1100

    Add Card Sample

--- src/common/components/Card/index.tsx
+++ src/common/components/Card/index.tsx
@@ -1,14 +1,14 @@
-import React, { FunctionComponent, ReactNode } from "react";
+import React, { FunctionComponent, CSSProperties } from "react";
 import styles from './styles.module.scss';
 import { WithChildren } from "../../types";
 
 type Props = {
-
+    style?: CSSProperties;
 } & WithChildren
 
 const Card: FunctionComponent<Props> = (props: Props) => {
     return (
-        <div className={styles.cardContainer}>
+        <div className={styles.cardContainer} style={props.style}>
             {props.children}
         </div>
     );
